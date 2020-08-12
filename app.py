92f61bd3af35b1fe0ca05d3c1f78e7caea0fe034
dirkkoolmees <65215053+dirkkoolmees@users.noreply.github.com>
2020-08-12 17:09:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def create_graphs(column, value):
     bar.update_xaxes(title_text='')
     bar.update_yaxes(title_text='')
     bar.update_layout(title = 'Top 15 Countries')
-    bar..update_traces(showscale=False)
+    bar.update_traces(showscale=False)
     return map, bar
 
 
